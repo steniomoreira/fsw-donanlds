d7e527672cdd1c1e5bc72af4e17bd23fab3482f4
steniomoreira <steniomoreiradev@gmail.com>
2025-02-20 23:38:07 -0300

    feat: add shadcn initial and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function ProductPage() {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products page</h1>
+
+      <Button>FSW</Button>
+    </div>
+  );
+}
+
+export default ProductPage;
