6a461a88de23777b714b45513ec6469fcf9afd9d
steniomoreira <steniomoreiradev@gmail.com>
2025-02-25 23:35:27 -0300

    feat: ensure cart summary is shown on reataurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -7,7 +7,10 @@ import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { formatCurrency } from "@/helpers/format-currency";
 
+import { useCartContext } from "../hooks/useCartContext";
+import CartSheet from "./cart-sheet";
 import Products from "./products";
 
 interface RestaurantCategoriesProps {
@@ -27,6 +30,8 @@ type MenuCategoriesWithProducts = Prisma.MenuCategoryGetPayload<{
 function RestaurantCategories({ restaurant }: RestaurantCategoriesProps) {
   const [selectedCategory, setSelectedCategory] =
     useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0]);
+
+  const { products, total, totalQuantity, toggleCart } = useCartContext();
 
   const handleCategoryClick = (categoty: MenuCategoriesWithProducts) => {
     setSelectedCategory(categoty);
@@ -76,6 +81,24 @@ function RestaurantCategories({ restaurant }: RestaurantCategoriesProps) {
 
       <h3 className="px-5 pt-4 font-semibold">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
+
+      {products.length > 0 && (
+        <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
+          <div>
+            <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+            <p className="text-sm font-semibold">
+              {formatCurrency(total)}
+              <span className="text-xs font-normal text-muted-foreground">
+                {" "}
+                / {totalQuantity} {totalQuantity > 1 ? "Itens" : "item"}
+              </span>
+            </p>
+          </div>
+
+          <Button onClick={toggleCart}>Ver sacola</Button>
+          <CartSheet />
+        </div>
+      )}
     </div>
   );
 }
